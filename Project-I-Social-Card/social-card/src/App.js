9f97daab5e48843363ab4b8a4a7f48dcbc96bb93
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-11 15:31:30 -0600

    header finished

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+import Footer from "./components/FooterComponents/Footer";
+import CardContainer from "./components/CardComponents/CardContainer";
+
 import "./App.css";
 
 const App = () => {
-  return <div>Hello There</div>;
+  return (
+    <div class="container">
+      <HeaderContainer />
+    </div>
+  );
 };
 
 export default App;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+
+const HeaderTitle = () => {
+  return (
+    <div class="headerTitle">
+      <h3>Lambda School</h3>
+      <p class="subtitle">@LambdaSchool - 26 jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
