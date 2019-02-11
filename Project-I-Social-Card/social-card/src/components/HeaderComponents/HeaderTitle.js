f4d3b206add184c244c770aa0ea99e5bfdccea04
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-11 16:22:25 -0600

    Took seconds off of moment

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -6,7 +6,7 @@ const HeaderTitle = () => {
     <div className="headerTitle">
       <h3>Lambda School</h3>
       <p className="subtitle">
-        @LambdaSchool - {moment().format("MMMM Do YYYY, h:mm:ss a")}
+        @LambdaSchool - {moment().format("MMMM Do YYYY, h:mm a")}
       </p>
     </div>
   );
