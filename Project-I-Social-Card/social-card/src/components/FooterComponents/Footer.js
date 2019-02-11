9521baa533cefc5f0559bfe6efce3cff625cb4bb
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-11 16:18:14 -0600

    footer functionality finished

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
+import "./Footer.css";
 
 export default class Footer extends Component {
   state = {
     shares: 0,
     likes: 0
   };
-  handleClick(e) {
-    console.log(e.target);
-  }
+  handleClick = e => {
+    if (e.target.id === "shareBtn") {
+      this.setState({
+        shares: this.state.shares + 1
+      });
+    } else {
+      this.setState({
+        likes: this.state.likes + 1
+      });
+    }
+  };
   render() {
     return (
       <div className="footer">
-        <i className="far fa-comment" />
+        <div className="comment">
+          <i className="far fa-comment" />
+        </div>
         <div className="share">
-          <i className="fas fa-sync" onClick={this.handleClick} />
+          <i className="fas fa-sync" id="shareBtn" onClick={this.handleClick} />
+          {this.state.shares}
         </div>
         <div className="like">
-          <i className="far fa-heart" onClick={this.handleClick} />
+          <i className="far fa-heart" id="likeBtn" onClick={this.handleClick} />
+          {this.state.likes}
         </div>
-        <i className="far fa-envelope" />
+        <div className="mail">
+          <i className="far fa-envelope" />
+        </div>
       </div>
     );
   }
